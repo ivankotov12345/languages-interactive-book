3bd32c8a6da78c3f3999be7fbb4642293d191a7c
Ivan Kotov <ivankotov12345@gmail.com>
2025-11-27 21:19:00 +0300

    feat(*): add api

---
+++ apps/api/src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const PORT = 3000;
+const MESSAGE = `server running on port ${PORT}`
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+    res.json({
+        status: 'OK',
+        message: MESSAGE
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(MESSAGE)
+});
